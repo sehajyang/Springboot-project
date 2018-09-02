b48947bedac2399e4172a7c75a69f8a2c9462dd3
yunjey0 <sehajyang@gmail.com>
2018-09-02 23:01:12 +0900

    Change jar > war, build Fix

--- src/main/java/com/spb/subject/DemoApplication.java
+++ src/main/java/com/spb/subject/DemoApplication.java
@@ -6,19 +6,30 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @MapperScan(value={"com.spb.subject"})
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+    {
+        return builder.sources(DemoApplication.class);
+    }
+    
     /*
      * SqlSessionFactory 설정
      */
